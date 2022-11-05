fc7433a54ec5d6f0691ac3521993a4a8f7087dfe
François Hétu <francois@arbrebinaire.com>
2022-11-04 20:20:17 -0400

    Déploiement dist du Fri Nov  4 08:20:17 PM EDT 2022

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
     css: ["@/assets/style/style.scss"],
     plugins: [{ src: '@/plugins/script.js' }],
     ssr: false,
+    meta: {
+        title: "L'Érablière || Chaque goutte compte"
+    },
     head: {
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     }
