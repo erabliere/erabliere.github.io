0d794807b00fbbe84ba426e2aae01816a6d30b6a
François Hétu <francois@arbrebinaire.com>
2022-11-07 07:36:10 -0500

    Déploiement dist du Mon Nov  7 07:36:10 AM EST 2022

--- plugins/script.js
+++ plugins/script.js
@@ -6,12 +6,18 @@ export default defineNuxtPlugin(nuxtApp => {
   // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
   function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
+    const clientHeight = window.innerHeight || document.documentElement.clientHeight
+/*     console.log('ch', clientHeight)
+    console.log('t', rect.top)
+    console.log('b', rect.bottom)
+    console.log('l', rect.left)
+    console.log('r', rect.right) */
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top <= clientHeight / 3
+      /*(window.innerHeight || document.documentElement.clientHeight)  &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) */
     );
   }
 
@@ -19,7 +25,7 @@ function callbackFunc() {
     const items = window.document.querySelectorAll(".timeline li.event");
 
     for (var i = 0; i < items.length; i++) {
-
+     // console.log(i)
       if (isElementInViewport(items[i])) {
         items[i].classList.add("in-view");
       }
